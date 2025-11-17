e7939101e55a04d34e2f496696dff8eb60906e46
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2025-11-17 12:36:06 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,32 @@
 Config = {}
 
+Config.BirdAttach = {
+    ["A_C_Hawk_01"] = {
+        Male = {296, 0.19, 0.01, 0.27, 0.0, 0.0, 0.0},
+        Female = {363, 0.12, -0.02, 0.27, 0.0, 0.0, 0.0}
+    },
+    ["A_C_Eagle_01"] = {
+        Male = {296, 0.19, 0.01, 0.27, 0.0, 0.0, 0.0},
+        Female = {363, 0.12, -0.02, 0.27, 0.0, 0.0, 0.0}
+    },
+    ["A_C_Crow_01"] = {
+        Male = {296, 0.15, 0.01, 0.25, 0.0, 0.0, 0.0},
+        Female = {363, 0.10, -0.02, 0.25, 0.0, 0.0, 0.0}
+    },
+    ["A_C_Vulture_01"] = {
+        Male = {296, 0.20, 0.02, 0.28, 0.0, 0.0, 0.0},
+        Female = {363, 0.13, -0.01, 0.28, 0.0, 0.0, 0.0}
+    },
+}
+
 -- General Settings
 Config.Debug           = false -- Dnable/disable debug functions
-Config.AllowSendToSelf = false -- A debug functions to allow sending letter to ourself (testing purpose)
+Config.AllowSendToSelf = true -- A debug functions to allow sending letter to ourself (testing purpose)
 Config.ChargePlayer    = true -- Whether to charge the player when sending a letter
 Config.CostPerLetter   = 0.50 -- Cost for sending a letter
 
 -- Bird Post settings
-Config.BirdModel        = 'A_C_Owl_01' -- Bird model to use
+Config.BirdModel        = 'A_C_Vulture_01' -- Bird model to use
 Config.AutoResurrect    = true -- Auto resurrect the bird when it's died while sending letters
 Config.BirdArrivalDelay = 20000 -- Set the bird to arrives after 20 secs
 Config.BirdTimeout      = 180 -- When timeout reached, the bird will fail to deliver the letter
@@ -107,6 +126,14 @@ Config.PostOfficeLocations = {
         name = 'Emerald Ranch Station Post Office',
         location = 'emeraldranch-postoffice',
         coords = vector3(1522.04, 439.54, 90.68),
+        blipsprite = 'blip_post_office', 
+        blipscale  = 0.2,
+        showblip = true
+    },
+	{
+        name = 'Benedictpoint Post Office',
+        location = 'Benedictpoint-postoffice',
+        coords = vector3(-5227.31, -3470.58, -20.57),
         blipsprite = 'blip_post_office', 
         blipscale  = 0.2,
         showblip = true
